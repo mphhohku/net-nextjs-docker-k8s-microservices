e6c1fc111b840d989789c97e8312ce8cdf0bf10e
mphhohku <130424415+mphhohku@users.noreply.github.com>
2023-09-28 09:27:50 +0800

    sep 14

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -55,9 +55,9 @@ public class AuctionsController : ControllerBase
         // TODO: add current user as seller
         auction.Seller = "test";
         _context.Auctions.Add(auction);
-        var result = await _context.SaveChangesAsync() > 0;
         var newAuction = _mapper.Map<AuctionDto>(auction);
         await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+        var result = await _context.SaveChangesAsync() > 0;
         if (!result)
         {
             return BadRequest("Could not save changes to the database.");
