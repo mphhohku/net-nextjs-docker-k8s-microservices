7aca8a03e43e667bef907c9537523dece1d5e64b
mphhohku <130424415+mphhohku@users.noreply.github.com>
2023-11-22 17:23:41 +0800

    Nov 22

---
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Register
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
